614a4340aaa28c50b62d3a64b15fe7739d0bc2fe
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-10-30 17:55:04 -0300

    criação de CarPostController, OwnerPostController e OwnerPostService

---
+++ api/src/main/java/com/joaoguilherme/api/controller/CarPostController.java
@@ -0,0 +1,36 @@
+package com.joaoguilherme.api.controller;
+
+import com.joaoguilherme.api.dto.CarPostDto;
+import com.joaoguilherme.api.service.CarPostStoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/car")
+public class CarPostController {
+
+    @Autowired
+    private CarPostStoreService carPostStoreService;
+
+    @GetMapping("/posts")
+    public ResponseEntity<List<CarPostDto>> getCarSales() {
+        return ResponseEntity.status(HttpStatus.FOUND).body(carPostStoreService.getCarForSales());
+    }
+
+    @PutMapping("/id")
+    public ResponseEntity changeCarSale(@RequestBody CarPostDto carPostDto, @PathVariable("id") String id) {
+        carPostStoreService.changeCarForSale(carPostDto, id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity deleteCarForSale(@PathVariable("id") String id) {
+        carPostStoreService.removeCarForSale(id);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+}
---
+++ api/src/main/java/com/joaoguilherme/api/controller/OwnerPostController.java
@@ -0,0 +1,27 @@
+package com.joaoguilherme.api.controller;
+
+
+import com.joaoguilherme.api.dto.OwnerPostDto;
+import com.joaoguilherme.api.service.OwnerPostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/owner")
+public class OwnerPostController {
+
+    @Autowired
+    private OwnerPostService ownerPostService;
+
+    @PostMapping
+    public ResponseEntity createOwnerCar (@RequestBody OwnerPostDto ownerPostDto) {
+        ownerPostService.createOwnerCar(ownerPostDto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
